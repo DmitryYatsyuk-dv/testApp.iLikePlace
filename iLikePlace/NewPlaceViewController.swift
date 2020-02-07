9a79c308a314d7f466b4ea5150f49326f092e2ae
DmitriyYatsyuk <yatsyuk.dev@mail.ru>
2020-02-07 13:21:43 +0300

    Save rating to database

--- iLikePlace/NewPlaceViewController.swift
+++ iLikePlace/NewPlaceViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class NewPlaceViewController: UITableViewController {
 
-    var currentPlace: Place?
+    var currentPlace: Place!
     var imageIsChanged = false
     
     @IBOutlet weak var saveButton: UIBarButtonItem!
@@ -19,7 +19,7 @@ class NewPlaceViewController: UITableViewController {
     @IBOutlet weak var placeName: UITextField!
     @IBOutlet weak var placeLocation: UITextField!
     @IBOutlet weak var placeType: UITextField!
-    
+    @IBOutlet weak var ratingControl: RatingControl!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -89,7 +89,10 @@ class NewPlaceViewController: UITableViewController {
         let newPlace = Place(name: placeName.text!,
                              location: placeLocation.text,
                              type: placeType.text,
-                             imageData: imageData)
+                             imageData: imageData,
+                             rating: Double(ratingControl.rating))
+        
+        
         
         if currentPlace != nil {
             try! realm.write {
@@ -97,6 +100,8 @@ class NewPlaceViewController: UITableViewController {
                 currentPlace?.location = newPlace.location
                 currentPlace?.type = newPlace.type
                 currentPlace?.imageData = newPlace.imageData
+                currentPlace?.rating = newPlace.rating
+                
             }
         } else {
             StorageManager.saveObject(newPlace)
@@ -117,6 +122,8 @@ class NewPlaceViewController: UITableViewController {
             placeName.text = currentPlace?.name
             placeLocation.text = currentPlace?.location
             placeType.text = currentPlace?.type
+            ratingControl.rating = Int(currentPlace.rating)
+            
         }
     }
     
--- iLikePlace/PlaceModel.swift
+++ iLikePlace/PlaceModel.swift
@@ -15,16 +15,19 @@ class Place: Object {
     @objc dynamic var type: String?
     @objc dynamic var imageData: Data?
     @objc dynamic var date = Date()
+    @objc dynamic var rating = 0.0
   
     convenience init(name: String,
                      location: String?,
                      type: String?,
-                     imageData: Data?) {
+                     imageData: Data?,
+                     rating: Double) {
         self.init()
         self.name = name
         self.location = location
         self.type = type
         self.imageData = imageData
+        self.rating = rating
          
     }
     
