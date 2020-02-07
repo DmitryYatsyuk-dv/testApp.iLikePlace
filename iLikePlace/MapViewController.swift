c22639f1bf77728829ac4622d4cd8c0ed418b7f7
DmitriyYatsyuk <yatsyuk.dev@mail.ru>
2020-02-07 14:17:30 +0300

    Added: MapKit

---
+++ iLikePlace/MapViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MapViewController.swift
+//  FavoritePlaces
+//
+//  Created by Lucky on 07/02/2020.
+//  Copyright © 2020 DmitriyYatsyuk. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    @IBAction func cancelAction(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+}
